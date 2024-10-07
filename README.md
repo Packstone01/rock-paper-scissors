bbb8c37980a21efe7cf2c678c5d61094a0f0bc68
Connor Schotz <140295366+Packstone01@users.noreply.github.com>
2024-10-07 13:03:22 -0400

    Finished the game.

--- README.md
+++ README.md
@@ -1,4 +1,16 @@
 # rock-paper-scissors
 
 Pseudocode for project:
-  
+  get computer input
+  get human input
+  compare computer vs human input
+    tie
+
+    rock vs paper - done
+    rock vs scissors - done
+    
+    paper vs rock
+    paper vs scissors
+
+    scissors vs rock
+    scissors vs paper
--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 let compChoice = '';
 let humanChoice = '';
 
-let humanScore = 0;
-let compScore = 0;
-
 function getComputerChoice() {
   num = Math.floor(Math.random() * 3) + 1;
   if (num === 1) {
@@ -15,19 +12,40 @@ function getComputerChoice() {
   }
 }
 
-getComputerChoice();
-console.log(compChoice);
-
 function getHumanChoice() {
   humanChoice = prompt('Do you choose rock, paper, or scissors? ');
 }
 
-getHumanChoice();
-humanChoice = humanChoice.toLowerCase();
-console.log(humanChoice);
+function playGame() {
+  let humanScore = 0;
+  let compScore = 0;
 
-function playRound(humanChoice, compChoice) {
-  if (humanChoice === compChoice) {
-    console.log('Tie game, you both chose ' + humanChoice);
+   function playRound() {
+     if (humanChoice === compChoice) {
+       console.log(`Tie game, you both chose ${humanChoice}.`);
+   } else if (humanChoice === 'rock' && compChoice === 'paper') {
+       console.log(`You lost! The computer chose paper.`);
+   } else if (humanChoice === 'rock' && compChoice === 'scissors') {
+       console.log(`You won! The computer chose scissors.`);
+   } else if (humanChoice === 'paper' && compChoice === 'scissors') {
+       console.log(`You lost! The computer chose scissors.`);
+   } else if (humanChoice === 'paper' && compChoice === 'rock') {
+       console.log(`You won! The computer chose rock.`);
+   } else if (humanChoice === 'scissors' && compChoice === 'rock') {
+       console.log(`You lost! The computer chose rock.`);
+   } else if (humanChoice === 'scissors' && compChoice === 'paper') {
+       console.log(`You won! The computer chose paper.`);
+    }
   }
+
+  const humanSelection = getHumanChoice();
+  const computerSelection = getComputerChoice();
+
+  playRound(humanSelection, computerSelection);
 }
+
+playGame();
+playGame();
+playGame();
+playGame();
+playGame();
